0f09c3c216cd1fc13b77942c7b0074fb0611e2bd
Maui Torio <mauiandsonsph@yahoo.com>
2020-03-24 17:41:51 +0800

    added withStyles from material-ui with demo styles

--- src/component/PaletteList.jsx
+++ src/component/PaletteList.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import MiniPalette from './MiniPalette';
+
 export default class PaletteList extends Component {
 	render() {
 		const { palettes } = this.props;
 		return (
 			<div>
 				<h1>React Colors</h1>
-				{palettes.map((palette) => (
-					<p>
-						<Link exact to={`/palette/${palette.id}`}>
-							{palette.paletteName}
-						</Link>
+				<MiniPalette />
+				{palettes.map((palette, idx) => (
+					<p key={idx}>
+						<Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
 					</p>
 				))}
 			</div>
